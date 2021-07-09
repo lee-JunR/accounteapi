689c0c0f00f8372aaaebdf69bc33c91b9832570d
lee-JunR <wns2610@icloud.com>
2021-07-09 09:26:17 +0900

    기존 회원정보 api화

---
+++ API/serializers.py
@@ -0,0 +1,42 @@
+# api/serializers.py
+from rest_framework import serializers
+from django.contrib.auth.models import User #장고 지원 모델의 User 모델 사용
+from django.contrib.auth import authenticate
+from .models import Profile
+# 회원가입
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data["username"], None, validated_data["password"]
+        )
+        return user
+
+
+# 접속 유지중인지 확인
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username")
+
+
+# 로그인
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Unable to log in with provided credentials.")
+
+# 프로필 시리얼라이저
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['user','image','dept','email','nickname','message','zonecode']
